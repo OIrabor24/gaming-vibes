fd006c61d09a3e987c1bd33e231882d33a72ec55
Osahon Irabor <osahonirabor@hotmail.com>
2023-02-22 17:26:24 -0800

    added login functionality

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import AuthContext from '../stores/authContext'
+import { useContext } from 'react'
 
 export default function Navbar() {
+  const {user, login, logout } = useContext(AuthContext);
+  console.log(user); 
+
   return (
     <div className="container">
       <nav>
-        <Image src="/rupee.png" width={50} height={48} />
+        <Image src="/rupee.png" width={50} height={48} alt="mario" priority="true" />
         <h1>Gaming Vibes</h1>
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/guides">Guides</Link></li>
+          <li onClick={login} className="btn">Login/Signup</li>
+          <li onClick={logout} className="btn">Logout</li> 
         </ul>
       </nav>
       <div className="banner">
